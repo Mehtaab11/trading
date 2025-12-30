b432bb87db2c5f93f6aa5e67a520374e4a7b1fc3
Mehtaab <mehtaabaalam117@gmail.com>
2025-12-30 19:22:38 +0530

    Initialise

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
-export default function Home() {
+import { Button } from "@/components/ui/button"
+
+const page = () => {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="flex justify-center items-center h-screen">
+      <Button> Click Me!</Button>
     </div>
-  );
+  )
 }
+
+export default page
