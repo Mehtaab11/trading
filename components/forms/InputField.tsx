08d5f8e600e2ffeec48cb5270dad5022248d0710
Mehtaab <mehtaabaalam117@gmail.com>
2026-01-12 01:28:45 +0530

    Implement Auth Logic

--- components/forms/InputField.tsx
+++ components/forms/InputField.tsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { cn } from "@/lib/utils";
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
+import {cn} from "@/lib/utils";
 
-const InputField = (
-    { name,
-        label,
-        placeholder,
-        type = "text",
-        register,
-        error,
-        validation,
-        disabled,
-        value }: FormInputProps) => {
+const InputField = ({ name, label, placeholder, type = "text", register, error, validation, disabled, value }: FormInputProps) => {
     return (
         <div className="space-y-2">
             <Label htmlFor={name} className="form-label">
